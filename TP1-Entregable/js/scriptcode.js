1acc0a647c845f822a8d27a98089ae5c0d804027
saantiago-sg <54604815+saantiago-sg@users.noreply.github.com>
2020-09-08 00:17:26 -0300

    Se agrego filtro de saturacion y iniciar un canvas de 0

--- TP1-Entregable/js/scriptcode.js
+++ TP1-Entregable/js/scriptcode.js
@@ -56,11 +56,10 @@ function deleteDraw(){
 }
 let btnDel = document.querySelector('#btnDelete').addEventListener('click', deleteDraw);
 
-//  AGREGAR IMAGEN
+//      AGREGAR IMAGEN
 
 let inputImage = document.querySelector(".inputImage");
 
-//canvas.addEventListener('click', colorGrey);
     inputImage.onchange = e => {
 
         let file = e.target.files[0];
@@ -92,17 +91,17 @@ let inputImage = document.querySelector(".inputImage");
     ctx.putImageData(imageData, 0, 0);
         }
     }
-}
+    }
 
-//FILTROS DE COLORES
+
+//      FILTROS DE COLORES
 
 function colorNegativo(){
 
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);    //obtengo los datos de la imagen, altura ancho y pos
     let pixeles = imageData.data;
     
-    for (var i = 0; i < pixeles.length; i += 4) { // recorre uno a uno los pixeles de la imagen y cambia el color por el complementario
-
+    for (let i = 0; i < pixeles.length; i += 4) { // recorre uno a uno los pixeles de la imagen y cambia el color por el complementario
         pixeles[i] = 255 - pixeles[i]; // rojo
         pixeles[i + 1] = 255 - pixeles[i + 1]; // verde
         pixeles[i + 2] = 255 - pixeles[i + 2]; // azul
@@ -116,7 +115,7 @@ function colorBrillo(){
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);    //obtengo los datos de la imagen, altura ancho y pos
     let pixeles = imageData.data;
    
-    for (var i = 0; i < pixeles.length; i += 4) { // recorre uno a uno los pixeles de la imagen y cambia el color por el complementario
+    for (let i = 0; i < pixeles.length; i += 4) { // recorre uno a uno los pixeles de la imagen y cambia el color por el complementario
         
         pixeles[i] = (255/3) + pixeles[i]; // rojo
         pixeles[i + 1] = (255/3) + pixeles[i + 1]; // verde
@@ -156,18 +155,95 @@ function colorSepia(){
         pixeles[ i * 4 + 1 ] = 255 - g;
         pixeles[ i * 4 + 2 ] = 255 - b;
  
-        pixeles[ i * 4 ] = ( r *.393 ) + ( g *.769 ) + ( b * .189 );
-        pixeles[ i * 4 + 1 ] = ( r * .349 ) + ( g *.686 ) + ( b * .168 );
-        pixeles[ i * 4 + 2 ] = ( r * .272 ) + ( g *.534 ) + ( b * .131 );
+        pixeles[ i * 4 ] = (r *.393) + (g *.769) + (b * .189);
+        pixeles[ i * 4 + 1 ] = (r * .349) + (g *.686) + (b * .168);
+        pixeles[ i * 4 + 2 ] = (r * .272) + (g *.534) + (b * .131);
     }
     ctx.putImageData(imageData, 0, 0);
 }
 let btnSepia = document.querySelector("#btnSepia").addEventListener("click", colorSepia);
 
+//      SATURACION 
+function saturacion(){
+    let imageData=ctx.getImageData(0,0,canvas.width,canvas.height);
+    let cantidad= 1.0;
+     for (y=0;y<canvas.height;y++){
+        for (x=0;x<canvas.width;x++){
+            let i =(x+y*imageData.width)*4;
+            let hsl=RGBaHSL(imageData.data[i+0],imageData.data[i+1],imageData.data[i+2]);
+            hsl[1]= cantidad;   //al ser el valor 1, elijo saturacion
+            let rgbNuevo=HSLaRGB(hsl[0],hsl[1],hsl[2]);
+            imageData.data[i+0]=rgbNuevo[0];
+            imageData.data[i+1]=rgbNuevo[1];
+            imageData.data[i+2]=rgbNuevo[2];
+        }
+     }
+    ctx.putImageData(imageData,0,0);
+}
+let btnSaturado = document.querySelector("#btnSaturacion").addEventListener("click", saturacion);
+
+function RGBaHSL(r, g, b){
+r /= 255, g /= 255, b /= 255; // divido la variable y le asigno el valor a la misma variable
+let max = Math.max(r,g,b);
+let min = Math.min(r,g,b);
+let hue, sat, li = (max + min) / 2;
+
+if(max == min){
+    hue = sat = 0; // no tiene saturacion
+}else{
+    let dif = max - min;
+    sat = li > 0.5 ? dif / (2 - max - min) : dif / (max + min);
+    switch(max){
+        case r: hue = (g - b) / dif;
+        break;
+        case g: hue = 2 + ((b - r) / dif);
+        break;
+        case b: hue = 4 + ((r - g) / dif);
+        break;
+    }
+    hue *= 60; //mult por 60 y se lo asigno a hue
+    if (hue < 0){
+        hue += 360;
+    }
+    }    
+    return([hue, sat, li]);
+
+}
+
+function HSLaRGB(hue , sat, li){
+if(sat === 0){
+    return[li, li, li];
+}
+hue /= 360; //num de saturacion
+let q = li < 0.5 ? li * (1 + sat) : li + sat - li * sat;
+let p = 2 * li - q;
+  return [Math.round(HUEaRGB(p, q, hue + 1/3) * 255),
+          Math.round(HUEaRGB(p, q, hue) * 255),
+          Math.round(HUEaRGB(p, q, hue - 1/3) * 255)]
+}
+
+function HUEaRGB (p, q, t) {
+    if (t < 0) t += 1
+    if (t > 1) t -= 1
+    if (t < 1/6) return p + (q - p) * 6 * t
+    if (t < 1/2) return q
+    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6
+  
+    return p;
+  }
+
+//      NINGUN FILTRO
+function ningunFiltro(){
+    alert("none"); 
+}
+let btnNinguno = document.querySelector("#btnNinguno").addEventListener("click", ningunFiltro);
+
+
+//      GUARDAR IMAGEN Y EMPEZAR NUEVO DIBUJO
 
 function guardarImage(){
-let link = document.createElement('a');
-let url = canvas.toDataURL();
+let link = document.createElement('a'); // creo un link para poder descargar la imagen q se creo con el nombre
+let url = canvas.toDataURL();           // imagecanvas 
 let filename = 'imagecanvas.jpg';
 link.setAttribute( 'href', url );
     link.setAttribute( 'download', filename );
@@ -177,3 +253,16 @@ link.setAttribute( 'href', url );
     window.document.body.removeChild( link );
 }
 let btnGuardar = document.querySelector("#btnGuardar").addEventListener("click", guardarImage);
+
+
+function nuevaImagen(){
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height); //dibuja un cuadrado
+
+let imageData = ctx.getImageData(0,0, canvas.width, canvas.height);
+
+ctx.putImageData(imageData, 0, 0);
+}
+let btnNewImage = document.querySelector("#btnNewImage").addEventListener("click", nuevaImagen);
+
+//
